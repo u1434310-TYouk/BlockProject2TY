baa436e55a5b871214d10dfeb696e0a85b302b7a
u1434310-TYouk <u1434310@gcloud.utah.edu>
2026-02-19 20:36:34 -0700

    Add files via upload

---
+++ pr.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp3;
+
+// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel;
+using System.Security.Cryptography;
+class program{
+    public static void Main(String[] args)
+    {
+        Random rnd = new Random();
+        String s = Console.ReadLine();
+
+        int hash = s.GetHashCode();
+
+        for (int i = 0; i < 13; i++)
+        {
+            if (hash % 2 == 0)
+                hash -= hash / rnd.Next(Math.Abs(hash));
+            else
+            {
+                hash -= hash / 2;
+            }
+        }
+
+        Console.WriteLine(hash + s.GetHashCode());
+    }
+}
